8ed02639261bf892de5e2a7e08208c1a452a8213
Мария Павлова <ivana.prego@gmail.com>
2025-05-18 18:49:24 +0300

    Create Program.cs

---
+++ Lab-SetsAndDictionariesAdvanced/1.CountSameValuesInArray/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _1.CountSameValuesInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> input=Console.ReadLine().Split(" ").Select(double.Parse).ToList();
+            Dictionary<double, int>values_count=new Dictionary<double, int>();
+            foreach (double number in input)
+            {
+                if (values_count.ContainsKey(number))
+                {
+                    values_count[number]++;
+                }
+                else
+                {
+                    values_count[number] = 1;
+                }
+            }
+            foreach (var (number, count) in values_count)
+            {
+                Console.WriteLine($"{number} - {count} times");
+            }
+        }
+    }
+}
