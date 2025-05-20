88bcd902b5f56b9c9d7822d27c4a097904918613
Мария Павлова <ivana.prego@gmail.com>
2025-05-20 15:47:00 +0300

    🩷

---
+++ Streams,FilesAndDirectories-Exercises/CopyDirectory/CopyDirectory.cs
@@ -0,0 +1,33 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.IO;
+
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath =  @$"{Console.ReadLine()}";
+            string outputPath = @$"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            //We have to delete the directory if it exists
+            //recursive:true-delete the directory even if it's not empty, otherwise it won't be deleted
+            if(Directory.Exists(outputPath)) Directory.Delete(outputPath, recursive:true);
+            //for every file in the directory, without sub-directories:
+            //1.F,irst get the name of the file
+            //2.Combine fileName with the output directory path
+            //3.Coppy the file to the destination 
+            foreach (string pathToFile in Directory.GetFiles(inputPath))
+            {
+                string fileName=Path.GetFileName(pathToFile);
+                string pathToDestinationFile=Path.Combine(outputPath, fileName);
+                File.Copy(pathToFile, pathToDestinationFile);
+            }
+        }
+    }
+}
---
+++ Streams,FilesAndDirectories-Exercises/ZipAndExtract/ZipAndExtract .cs
@@ -0,0 +1,52 @@
+﻿namespace ZipAndExtract
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+
+    public class ZipAndExtract
+    {
+        static void Main()
+        {
+            string inputFile = @"..\..\..\copyMe.png";
+            string zipArchiveFile = @"..\..\..\archive.zip";
+            string extractedFile = @"..\..\..\extracted.png";
+
+            ZipFileToArchive(inputFile, zipArchiveFile);
+
+            var fileNameOnly = Path.GetFileName(inputFile);
+            ExtractFileFromArchive(zipArchiveFile, fileNameOnly, extractedFile);
+        }
+
+        public static void ZipFileToArchive(string inputFilePath, string zipArchiveFilePath)
+        {
+            //If zipFile with this nsme exist=delete it
+            if(File.Exists(zipArchiveFilePath)) {File.Delete(zipArchiveFilePath); }
+            //Using ZipArchive to create zipFile:
+            //1. Get name if the input lile
+            //2. Create archive
+            using (ZipArchive archive = ZipFile.Open(zipArchiveFilePath, ZipArchiveMode.Create))
+            {
+                string name=Path.GetFileName(inputFilePath);
+                archive.CreateEntryFromFile(inputFilePath, name); 
+            }
+        }
+
+        public static void ExtractFileFromArchive(string zipArchiveFilePath, string fileName, string outputFilePath)
+        {
+            //Open zipArchive, to read zip file
+            using (ZipArchive archive = ZipFile.Open(zipArchiveFilePath, ZipArchiveMode.Read))
+            {
+                //Get element from the archive with the same name, if it doesn't exist, thow exception
+                ZipArchiveEntry entry = archive.GetEntry(fileName);
+                if (entry is null)
+                {
+                    throw new InvalidOperationException("Invalide file name provided!");
+
+                }
+                //Extract the zip file to the output file
+                entry.ExtractToFile(outputFilePath);
+            }
+        }
+    }
+}
