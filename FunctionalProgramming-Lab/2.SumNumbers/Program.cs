771d64718dac024a588102b3a0a471ebfd13aa00
Мария Павлова <ivana.prego@gmail.com>
2025-05-20 20:06:12 +0300

    ☆*: .｡. o(≧▽≦)o .｡.:*☆🩷

---
+++ FunctionalProgramming-Lab/2.SumNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _2.SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+            Console.WriteLine(numbers.Count);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
