fecb1cf54de3ac6f0b1f527fad883929b6a7be85
Мария Павлова <ivana.prego@gmail.com>
2025-06-23 11:43:47 +0300

    ⭐

---
+++ Exercises-Functional Programming/07.PredicateForNames/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Sockets;
+
+namespace _07.PredicateForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+             Filter(names, name=>name.Length<=n, name=>Console.WriteLine(name) );
+            
+
+
+
+        }
+        static void Filter(string[] names, Predicate<string> filter, Action<string>action)
+        {
+           
+            foreach(string name in names)
+            {
+                if (filter(name))
+                {
+                    action(name);
+                }
+            }
+            
+            
+        }
+    }
+}
---
+++ Exercises-Functional Programming/08.ListOfPredicates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _08.ListOfPredicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=int.Parse(Console.ReadLine());
+            int[]dividers=Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            Func<int, bool>[] conditions = new Func<int, bool>[dividers.Length];
+            for (int i = 0; i < dividers.Length; i++)
+            {
+                int pos = i;
+                conditions[pos]=x => x%dividers[pos]==0;
+            }
+            var result = Filter(1, n, conditions);
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        static List<int>Filter(int start, int end, Func<int, bool>[] conditions)
+        {
+            List<int> result = new List<int>();
+            
+            for (int i = start; i <=end; i++)
+            {
+                bool isMath = true;
+                foreach (var c in  conditions)
+                {
+                    if (!isMath) break;
+                   isMath = c(i);
+                }
+                if(isMath)result.Add(i);
+            }
+            return result;
+        }
+    }
+}
