95ee7a92af9ada33d9cbcacfc9781d3d0cdefd4a
Мария Павлова <ivana.prego@gmail.com>
2025-06-26 18:56:54 +0300

    📅🤔⁉️

---
+++ DefiningClasses-Exercise/DateModifier/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace DateModifier;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        string startDate=Console.ReadLine();
+        string endDate=Console.ReadLine();  
+        DateModifier dateModifier = new DateModifier();
+        dateModifier.DaysCounter(startDate, endDate);
+    }
+}
