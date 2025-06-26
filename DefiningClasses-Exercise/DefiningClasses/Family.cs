a2bf0652d40ac48cf985c114dbb7c867842e806d
Мария Павлова <ivana.prego@gmail.com>
2025-06-26 18:42:46 +0300

    🐹

---
+++ DefiningClasses-Exercise/DefiningClasses/Family.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public List<Person> Members = new();
+        public void AddMember(Person person)
+        {
+            Members.Add(person);
+        }
+        public Person GetOldestMember()
+        {
+           return Members.OrderByDescending(p=>p.Age).First();
+        }
+
+    }
+}
