a33dd3a21c4df00a7ae341e111251dc92ec6f890
Мария Павлова <ivana.prego@gmail.com>
2025-06-27 18:00:57 +0300

    💛

---
+++ DefiningClasses-Exercise/PokemonTrainer/Trainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonTrainer
+{
+    public  class Trainer
+    {
+        public string Name {  get; set; }
+        public int NumberOfBadges { get; set; } = 0;
+        public List<Pokemon> Pokemons = new();
+
+        public void RemoveDeadPokemons()
+        {
+            this.Pokemons.RemoveAll(p=>p.Health<=0);    
+        }
+    }
+}
