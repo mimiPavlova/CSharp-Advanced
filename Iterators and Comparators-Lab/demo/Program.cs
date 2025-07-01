70830f1d6f5f5b18e5821b4cc6c01b8f07f494b8
Мария Павлова <ivana.prego@gmail.com>
2025-07-01 21:04:31 +0300

    😵‍💫

---
+++ Iterators and Comparators-Lab/demo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fragment f = new Fragment("Hello, world");
+
+            foreach (var item in f)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
