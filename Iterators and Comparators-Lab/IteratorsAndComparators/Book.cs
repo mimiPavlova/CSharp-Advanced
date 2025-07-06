25b9ebf9781a6872268cb79848577981097fd665
Мария Павлова <ivana.prego@gmail.com>
2025-07-06 22:38:47 +0300

    😿

---
+++ Iterators and Comparators-Lab/IteratorsAndComparators/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators;
+
+public class Book:IComparable<Book>
+{
+    private readonly string _title;
+    private readonly int _year;
+    private readonly List<string> _authors;
+
+    public string Title => this._title;
+
+    public int Year => this._year;
+    public List<string> Author => this._authors;
+
+    public Book(string title, int year, params string[] authours)
+    {
+        _title = title;
+        _year = year;
+        _authors=new List<string>(authours);
+    }
+
+    public int CompareTo(Book? other)
+    {
+        int comparison = this.Year.CompareTo(other.Year);
+       if(comparison == 0) 
+        {
+            return this.Title.CompareTo(other.Title);
+        }
+       return comparison;
+    }
+    public override string ToString() => $"{Title} - {Year}";
+}
---
+++ Iterators and Comparators-Lab/IteratorsAndComparators/Library.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators;
+
+public class Library : IEnumerable<Book>
+{
+    private readonly List<Book> _books;
+    public List<Book> Books => this._books;
+
+    public Library(params Book[] books)
+    {
+        //this._books=new List<Book>(books.OrderBy(b=>b));
+        this._books=new List<Book>(books);
+        this._books.Sort(new BookComparator());
+    }
+
+    public IEnumerator<Book> GetEnumerator()
+    {
+        return new LibraryIterator(_books);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+
+    private class LibraryIterator : IEnumerator<Book>
+    {
+
+        private readonly List<Book> _books;
+        int index;
+        public LibraryIterator(List<Book> books)
+        {
+            this._books = books;
+            Reset();
+        }
+        public Book Current => _books[index];
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+            
+        }
+
+        public bool MoveNext()
+        {
+            if (index>=this._books.Count) return false;
+            else
+            {
+                return ++index<this._books.Count;
+            }
+        }
+
+        public void Reset()
+        {
+            index=-1;
+        }
+    }
+}
