9f91af9eb55d01a7caf664d855ab61bbd9878f14
Мария Павлова <ivana.prego@gmail.com>
2025-07-08 12:39:21 +0300

    😺

---
+++ Exercise-IteratorsAndComparators/06.EqualityLogic/Person.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.EqualityLogic
+{
+    public class Person : IComparable<Person>, IEquatable<Person>
+    {
+        private readonly string _name;
+        private readonly int _age;
+        public Person(string name, int age)
+        {
+            _name=name;
+            _age=age;
+
+        }
+
+        public int CompareTo(Person? other)
+        {
+            if (other is null)
+                throw new ArgumentNullException();
+
+            int nameComparison = this._name.CompareTo(other._name);
+            if (nameComparison!=0) return nameComparison;
+
+            int ageComparison = this._age.CompareTo(other._age);
+            return ageComparison;
+
+
+        }
+
+
+        public override bool Equals(object obj)
+       => obj is Person && Equals((Person)obj);
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_name.GetHashCode(), _age);
+        }
+
+        public bool Equals(Person? other)
+        {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+            else return EqualityComparer<string>.Default.Equals(this._name, other._name)
+            &&EqualityComparer<int>.Default.Equals(this._age, other._age);
+        }
+
+        public static bool operator ==(Person left, Person right)
+        => EqualityComparer<Person>.Default.Equals(left, right);
+
+        public static bool operator !=(Person left, Person right)
+        {
+            return !(left==right);
+        }
+
+        public static bool operator <(Person left, Person right)
+        {
+            return Comparer<Person>.Default.Compare(left, right) < 0;
+        }
+
+        public static bool operator <=(Person left, Person right)
+        {
+            return Comparer<Person>.Default.Compare(left, right) <=0;
+        }
+
+        public static bool operator >(Person left, Person right)
+        {
+            return Comparer<Person>.Default.Compare(left, right) > 0; ;
+        }
+
+        public static bool operator >=(Person left, Person right)
+        {
+            return Comparer<Person>.Default.Compare(left, right) >= 0;
+        }
+    }
+}
---
+++ Exercise-IteratorsAndComparators/06.EqualityLogic/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _06.EqualityLogic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<Person> sortedSet = new SortedSet<Person>();
+            HashSet<Person> set = new HashSet<Person>();
+            int n=int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] data = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Person person=new Person(data[0], int.Parse(data[1]));
+                sortedSet.Add(person);
+                set.Add(person);
+            }
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(set.Count);
+        }
+    }
+}
