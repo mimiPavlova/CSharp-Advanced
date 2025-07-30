2a17a5acd79b0a8dcb4f05fb115723d3a1df34e1
Мария Павлова <ivana.prego@gmail.com>
2025-07-30 20:32:01 +0300

    💛

---
+++ ExamPrep3/01.ClickBait/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace _01.ClickBait
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> q = new Queue<int>(Console.ReadLine().Split(" ").Select(int.Parse));
+            Stack<int> s = new Stack<int>(Console.ReadLine().Split(" ").Select(int.Parse).ToArray());
+
+            List<int>result=new List<int>();
+            int targetValue=int .Parse(Console.ReadLine());
+
+            while (q.Count>0&&s.Count>0)
+            {
+                int queueElement = q.Dequeue();
+                int stackElement = s.Pop();
+
+                if(stackElement>queueElement)
+                {
+                    int remainder=stackElement%queueElement;
+                    result.Add(Math.Abs(remainder));
+                    if (remainder>0)
+                    {
+                        s.Push(remainder*2);
+                    }
+                }
+                else if (queueElement>stackElement)
+                {
+                    int remainder=queueElement%stackElement;
+                    result.Add(0-remainder);
+                    if (remainder>0)
+                    {
+                        q.Enqueue(remainder*2);
+                    }
+
+                }
+                else if (queueElement==stackElement)
+                {
+                    result.Add(0);
+                }
+
+
+
+            }
+            Console.WriteLine($"Final Feed: {string.Join(", ", result)}");
+            int totalEngagementValue=result.Sum();
+            if (totalEngagementValue>=targetValue)
+            {
+                Console.WriteLine($"Goal achieved! Engagement Value: {totalEngagementValue}");
+            }
+            else
+            {
+                int shortfall=targetValue-totalEngagementValue;
+                Console.WriteLine($"Goal not achieved! Short by: {shortfall}");
+            }
+           
+        }
+    }
+}
---
+++ ExamPrep3/02.SpaceMission/Program.cs
@@ -0,0 +1,112 @@
+﻿namespace _02.SpaceMission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=int.Parse(Console.ReadLine());
+            char[,]matrix=new char[n, n];
+            int startRow = 0, startCol = 0;
+            int row = 0, col = 0;
+            for (int i = 0; i < n; i++)
+            {
+               char[]input=Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j]=input[j];
+                    if (matrix[i, j]=='S')
+                    {
+                        startRow=i; startCol=j; 
+                        row=i; col=j;
+                    }
+                }
+            }
+            int resources = 100;
+            bool isOnTheMap=true;
+            bool victory = false;
+            while (resources>=5&&isOnTheMap&&!victory)
+            {
+                int nextRow = row, nextCol = col;
+                string command = Console.ReadLine();
+
+                if (command=="up") nextRow--;
+                else if (command=="down") nextRow++;
+                else if (command=="right") nextCol++;
+                else if (command=="left") nextCol--;
+
+                if (nextRow<0||nextRow>=n||nextCol<0||nextCol>=n)
+                {
+                    isOnTheMap = false;
+                }
+                else
+                {
+                    col = nextCol;
+                    row = nextRow;
+                    resources-=5;
+
+                    if (matrix[nextRow, nextCol]=='R')
+                    {
+                        resources+=10;
+                        if (resources>100) resources=100;
+                    }
+                    else if (matrix[nextRow, nextCol]=='M')
+                    {
+                        resources-=5;
+                        matrix[nextRow, nextCol]='.';
+                        if (resources<5) break;
+                        
+                    }
+                    else if (matrix[nextRow, nextCol]=='P')
+                    {
+                        victory = true;
+                    }
+                   
+                    
+                } 
+            }
+
+
+            if (victory)
+            {
+                Console.WriteLine($"Mission accomplished! The spaceship reached Planet Eryndor with {resources} resources left.");
+            }
+            else
+            {
+
+
+
+                if (!isOnTheMap)
+                {
+                    Console.WriteLine("Mission failed! The spaceship was lost in space.");
+                }
+                else 
+                {
+                    Console.WriteLine("Mission failed! The spaceship was stranded in space.");
+                }
+          
+                   matrix[row, col]='S';
+            
+            }
+              
+            matrix[startRow, startCol]='.';
+            
+
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    Console.Write(matrix[r, c]);
+
+                 
+                    if (c < matrix.GetLength(1) - 1)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine(); 
+            }
+        }
+
+       
+    }
+}
