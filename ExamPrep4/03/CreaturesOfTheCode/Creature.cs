307d31870acfec45035e933d4009a68ced08b616
Мария Павлова <ivana.prego@gmail.com>
2025-07-31 12:25:26 +0300

    🐉

---
+++ ExamPrep4/03/CreaturesOfTheCode/Creature.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace CreaturesOfTheCode
+{
+    public class Creature
+    {
+      public string Name {  get; set; }
+        public string Kind {  get; set; }
+        public int Health {  get; set; }
+        private List<string> abilities;
+        public List<string> Abilities=>abilities;
+
+        public Creature(string name, string kind, int health,  string abilities)
+        {
+            Name=name;
+            Kind=kind;
+            Health=health;
+            this.abilities=abilities.Split(", ").ToList();        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Name} ({Kind}) has {Health} HP");
+            sb.AppendLine($"Abilities: {string.Join(", ", abilities)}");
+            return sb.ToString().Trim();
+        }
+
+    }
+}
---
+++ ExamPrep4/03/CreaturesOfTheCode/MythicalCreaturesHub.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+
+namespace CreaturesOfTheCode
+{
+    public class MythicalCreaturesHub
+    {
+        private List<Creature> _creatures;
+        public List<Creature> Creatures => _creatures;
+        public int Capacity { get; set; }
+        public MythicalCreaturesHub(int capacity)
+        {
+            Capacity = capacity;
+            _creatures = new List<Creature>();
+        }
+        public void AddCreature(Creature creature)
+        {
+            if (!_creatures.Any(c => c.Name.Equals(creature.Name, StringComparison.OrdinalIgnoreCase)&&creature!=null))
+            {
+                if (_creatures.Count<Capacity) 
+                _creatures.Add(creature);
+            }
+        }
+        public bool RemoveCreature(string name)
+        {
+            Creature creature = _creatures.FirstOrDefault(c => c.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+            if (creature!=null)
+            {
+                _creatures.Remove(creature);
+                return true;
+            }
+            return false;
+        }
+        public Creature GetStrongestCreature()
+        {
+            return _creatures.OrderByDescending(c => c.Health).FirstOrDefault();
+        }
+
+        public string Details(string creatureName)
+        {
+            Creature creature = _creatures.FirstOrDefault(c => c.Name==creatureName);
+            if (creature is null)
+            {
+                return $"Creature with the name {creatureName} not found.";
+            }
+            return creature.ToString();
+        }
+        public string GetAllCreatures()
+        {
+            var sorted = _creatures.OrderBy(c => c.Name);
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("Mythical Creatures:");
+            foreach (var creature in sorted)
+            {
+
+                builder.AppendLine($"{creature.Name} -> {creature.Kind}");
+
+            }
+            return builder.ToString().Trim();
+        }
+
+
+    }
+
+
+}
