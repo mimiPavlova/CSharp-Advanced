b29c521b2f3478da2a1f88cabac13efd467bb4fc
Мария Павлова <ivana.prego@gmail.com>
2025-08-02 13:15:39 +0300

    👗💙🖤

---
+++ ExamPrep5/03/Cloth.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace ClothesMagazine
+{
+    public class Cloth
+    {
+        public string Color {  get; set; }
+        public int Size {  get; set; }
+        public string Type { get; set; }
+
+        public Cloth(string color, int size, string type)
+        {
+            Color=color;
+            Size=size;
+            Type=type;
+        }
+        public override string ToString()
+        => $"Product: {Type} with size {Size}, color {Color}";
+    }
+}
---
+++ ExamPrep5/03/Magazine.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ClothesMagazine
+{
+    public class Magazine
+    {
+        public string Type { get; set; }
+        public int Capacity {  get; set; }
+        private List<Cloth>_clothes;
+        public List<Cloth> Clothes=>_clothes;
+
+        public Magazine(string type, int capacity)
+        {
+            Type=type;
+            Capacity=capacity;
+
+            _clothes=new List<Cloth>();
+        }
+        public void AddCloth(Cloth cloth)
+        {
+            if (_clothes.Count<Capacity)
+            {
+                _clothes.Add(cloth);
+            }
+        }
+        public bool RemoveCloth(string color)
+        {
+            Cloth cloth=_clothes.FirstOrDefault(c=>c.Color==color);
+            if (cloth!=null)
+            {
+                _clothes.Remove(cloth);
+                return true;
+            }
+            return false;
+        }
+        public Cloth GetSmallestCloth()
+        {
+            return _clothes.OrderBy(c => c.Size).FirstOrDefault();
+        }
+        public Cloth GetCloth(string color)
+        {
+            return _clothes.FirstOrDefault(c=>c.Color==color);
+        }
+        public int GetClothCount()=>_clothes.Count;
+
+        public string Report()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($" {Type} magazine contains:");
+            var result=_clothes.OrderBy(c=>c.Size).ToList();
+            foreach (Cloth item in result)
+            {
+                stringBuilder.AppendLine(item.ToString());
+            }
+            return stringBuilder.ToString().Trim();    
+        }
+
+
+    }
+}
